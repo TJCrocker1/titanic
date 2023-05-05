48b68f360c0bda0670da2d053b5ccaac86261423
TJCrocker1 <TJCrocker1@gmail.com>
2023-05-05 23:19:37 +0100

    Tested New Classifiers
    Several classification algorythems were tested: Random forest, KNN, Naive Gaussian Bayes, Perceptron, Linear SVM, Logistic Regression & Decision Tree.
    
    The feature engineering script was also updated. Prefix classification was improved and the boudries of age and fare groups were adjusted and a new variable -family size- was added. The marrital status and presence of depenants / spouse aboard were removed.
    
    The Random forrest classifier appeard to perform best when compared against the trainning data set but did not imporve performace in the test set.

--- clean.R
+++ clean.R
@@ -6,12 +6,33 @@ require(magrittr)
 #-----------------------------------------------------------------------------------------------------------------------
 # funcitons:
 #-----------------------------------------------------------------------------------------------------------------------
-extract_prefix <- function(name) {
-  stringr::str_extract_all(name, "(Mme)|([mM]iss)|([Mm]s)|([Mm]r)|([Mm]aster)|(Dr)|(Rev)|(Col)|(Major)|(Sir)|(Lady)")
+extract_prefix <- function(name, sex) {
+  purrr::map2_chr(name, sex, ~{
+    #prefix <- stringr::str_extract_all(., "(Mme)|(Mlle. )|([mM]iss)|([Mm]rs)|([Mm]r)|([Mm]aster)|(Dr)|(Rev)|(Col)|(Major)|(Capt. )|(Don. )|(Sir)|(Lady)|(Countess. )|(Jonkheer. )")
+
+    # if Nob or prof, rare
+    prof <- stringr::str_detect(.x, "(Dr)|(Rev)|(Col)|(Major)|(Capt. )")
+    nob <- stringr::str_detect(.x, "(Don. )|(Dona.)|(Sir)|(Lady)|(Countess. )|(Jonkheer. )")
+    if(nob | prof) { return("rare") }
+    
+    # if married female, Mrs 
+    # (inc professional females - these are likely to be assuming their husbands title because sexism and all that, right?)
+    if(stringr::str_detect(.x, "(Mlle. )|([Mm]rs)") | (prof & .y == 1) | (stringr::str_detect(.x, "Mr") & .y == 1)) {return("Mrs")}
+    
+    # If professional, Prof
+    if(prof) {return("prof")}
+      
+    # if unmarried female, Miss
+    if(stringr::str_detect(.x, "(Mme)|([mM]iss)")) {return("Miss")}
+    
+    if(stringr::str_detect(.x, "Mr")) {return("Mr")}
+    if(stringr::str_detect(.x, "Master")) {return("Master")}
+    if(stringr::str_detect(.x, "[Mm]s")) {return("Ms")}
+  })
 }
 
-extract_prefix(titanic_train$Name[760])
-
+#extract_prefix(titanic$name, titanic$sex)
+# Mr, Mrs, Miss, Master, Prof, Nob
 
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -97,32 +118,44 @@ titanic <- titanic %>%
 
 titanic <- titanic %>%
   dplyr::mutate(
-    prefix = stringr::str_extract_all(titanic$name, "(Mme)|([mM]iss)|([Mm]s)|([Mm]r)|([Mm]aster)|(Dr)|(Rev)|(Col)|(Major)|(Sir)|(Lady)"),
+    prefix = extract_prefix(name, sex),
     family_size = sib_sp + parch,
-    married = purrr::map_int(prefix, ~{stringr::str_detect(., "(Mr)|(Mrs)|(Mme)") %>% any()}),
-    with_spouse = as.integer( married & sib_sp > 0 ),
-    with_dependants = as.integer( married & parch > 0 ),
-    socialite = purrr::map_int(prefix, ~{stringr::str_detect(., "(Rev)|(Dr)|(Col)|(Major)|(Sir)|(Lady)") %>% any()}),
+    #married = purrr::map_int(prefix, ~{stringr::str_detect(., "(Mr)|(Mrs)|(Mme)") %>% any()}),
+    #with_spouse = as.integer( married & sib_sp > 0 ),
+    #with_dependants = as.integer( married & parch > 0 ),
+    #socialite = purrr::map_int(prefix, ~{stringr::str_detect(., "(Rev)|(Dr)|(Col)|(Major)|(Sir)|(Lady)") %>% any()}),
     fare_bracket = purrr::map_chr(fare, function(fare) {
-      if(fare < 30) return("low")
-      if(fare < 100) return("med")
-      if(fare < 200) return("high")
-      return("very_high")
+      if(fare < 25) return("Fare_lowest")
+      if(fare < 50) return("Fare_low")
+      if(fare < 100) return("Fare_med")
+      if(fare < 200) return("Fare_high")
+      return("Fare_very_high")
     }),
     age_bracket = purrr::map_chr(age, function(age) {
-      if(age < 5) return("infant")
       if(age < 12) return("child")
       if(age < 20) return("teenager")
       if(age < 40) return("adult")
       return("old")
     })
   ) %>%
-  dplyr::select(PassengerId, survived, class, sex, age, fare, deck, embarked, family_size, married, with_spouse, with_dependants, socialite, fare_bracket, age_bracket, set)
+  dplyr::select(PassengerId, survived, prefix, class, sex, age, fare, deck, embarked, family_size, fare_bracket, age_bracket, set)
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 # possible feature engineering approaches:
 #-----------------------------------------------------------------------------------------------------------------------
+
+titanic %>%
+  dplyr::mutate(fare = round(titanic$fare) ) %>%
+  dplyr::arrange(fare) %>%
+  dplyr::group_by(fare) %>%
+  dplyr::summarise(
+    n = dplyr::n()
+  ) %>%
+  ggplot2::ggplot( ggplot2::aes( fare, n ) ) +
+  ggplot2::geom_col() +
+  ggplot2::scale_x_continuous(breaks = seq(0, 500, 25)) +
+  ggplot2::coord_cartesian(xlim = c(0, 50))
 
 # family size:
 # parch + sib_sp
--- svm.py
+++ svm.py
@@ -1,10 +1,20 @@
 #------------------------------------------------- svm for titanic (kaggle) --------------------------------------------
 import pandas
+import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn import svm
 from sklearn import metrics
 import numpy
+
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
 
 #import matplotlib.pyplot as plt
 
@@ -20,7 +30,12 @@ titanic = pandas.read_csv("titanic/titanic_clean.csv")
 #-----------------------------------------------------------------------------------------------------------------------
 
 # make array for processed data: (include cols that don't need pre-processing)
-processed = titanic[["survived", "sex", "married", "with_spouse", "with_dependants", "socialite", "set"]].copy()
+processed = titanic[["survived", "sex", "set"]].copy()
+
+# one hot encodeing - prefix
+ohe = OneHotEncoder()
+prefix_trans = ohe.fit_transform(titanic[["prefix"]])
+processed[ohe.categories_[0]] = prefix_trans.toarray().astype(int)
 
 # one hot encodeing - class
 ohe = OneHotEncoder()
@@ -60,25 +75,137 @@ processed["port_"+ohe.categories_[0][:3]] = numpy.delete(embarked_trans.toarray(
 #print(processed.to_string())
 
 #-----------------------------------------------------------------------------------------------------------------------
-# split into X, Y and fit model
+# split into X, Y and fit models
 #-----------------------------------------------------------------------------------------------------------------------
-train_X = processed[processed.set == "train"].drop(["survived", "set"], axis = 1)
-train_Y = processed[processed.set == "train"]["survived"].copy()
+X_train = processed[processed.set == "train"].drop(["survived", "set"], axis = 1)
+Y_train = processed[processed.set == "train"]["survived"].copy()
 
-mod = svm.SVC(kernel = 'poly', gamma = 1, C = 1)
-mod.fit(train_X, train_Y)
-pred = mod.predict(train_X)
 
-print("Accuracy:",metrics.accuracy_score(train_Y, pred))
+# support vector machine:
+svm = SVC(kernel = 'poly')
+svm.fit(X_train, Y_train)
+acc_svm = round(svm.score(X_train, Y_train)*100, 2)
+#print("Accuracy:", svm.score(X_train, Y_train))
 
+# Stocastic Gradient Decent:
+#sgd = linear_model.SGDClassifier(max_iter=5, tol = None)
+#sgd.fit(X_train, Y_train)
+#sgd.score(X_train, Y_train)
+
+#acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+
+# Random Forest:
+rf = RandomForestClassifier(n_estimators=1000)
+rf.fit(X_train, Y_train)
+rf.score(X_train, Y_train)
+
+#acc_rf = round(rf.score(X_train, Y_train)*100, 2)
+
+# Logistic regression:
+#logreg = LogisticRegression()
+#logreg.fit(X_train, Y_train)
+
+#acc_logreg = round(logreg.score(X_train, Y_train)*100, 2)
+
+# K Nearest Neighbor:
+#knn = KNeighborsClassifier(n_neighbors=3)
+#knn.fit(X_train,Y_train)
+
+#acc_knn = round(knn.score(X_train, Y_train)*100, 2)
+
+# Gaussian Naive Bayes:
+#gnb = GaussianNB()
+#gnb.fit(X_train, Y_train)
+#acc_gnb = round(gnb.score(X_train, Y_train)*100, 2)
+
+# Perceptron
+#prcy = Perceptron(max_iter=5)
+#prcy.fit(X_train, Y_train)
+
+#acc_prcy = round(prcy.score(X_train, Y_train)*100, 2)
+
+# linear svm:
+#lsvm = LinearSVC()
+#lsvm.fit(X_train, Y_train)
+
+#acc_lsvm = round(lsvm.score(X_train, Y_train)*100, 2)
+
+# Decision Tree
+#dtree = DecisionTreeClassifier()
+#dtree.fit(X_train, Y_train)
+
+#acc_dtree = round(dtree.score(X_train, Y_train)*100, 2)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# print results:
+#-----------------------------------------------------------------------------------------------------------------------
+#results = pandas.DataFrame({
+#    'Model': ["SVM", "Stockastic Gradient Decent", "Random Forest",
+#              "Logistic Regression", "K Nearest Neighbors", "Gaussian Naive Bays",
+#              "Perceptron", "Linear SVM", "Decision Tree"],
+#    "Score": [acc_svm, acc_sgd, acc_rf,
+#              acc_logreg, acc_knn, acc_gnb,
+#              acc_prcy, acc_lsvm, acc_dtree]
+#}).sort_values(by = 'Score', ascending=False)
+
+#print(results.to_string())
+
+#-----------------------------------------------------------------------------------------------------------------------
+# k-folds cross validation
+#-----------------------------------------------------------------------------------------------------------------------
+#from sklearn.model_selection import cross_val_score
+#rf_scores = cross_val_score(rf, X_train, Y_train, cv = 10, scoring="accuracy")
+#print(rf_scores)
+#print("Mean:", rf_scores.mean(), " SD:", rf_scores.std())
+
+#svm_scores = cross_val_score(rf, X_train, Y_train, cv = 10, scoring="accuracy")
+#print(svm_scores)
+#print("Mean:", svm_scores.mean(), " SD:", svm_scores.std())
+
+
+rel_importance = pd.DataFrame({
+    'feature':X_train.columns,
+    'importance':numpy.round(rf.feature_importances_, 3)
+}).sort_values('importance', ascending=False)
+
+print(rel_importance.to_string())
+
+#-----------------------------------------------------------------------------------------------------------------------
+# try random forrest with fewer params
+#-----------------------------------------------------------------------------------------------------------------------
+X_train_red = X_train.drop(["Fare_high", "Fare_highest", "Ms", "deck_T", "deck_F", "deck_G"], axis=1)
+
+rf_red = RandomForestClassifier(n_estimators=100, oob_score=True)
+rf_red.fit(X_train_red, Y_train)
+acc_rf_red = round(rf_red.score(X_train_red, Y_train)*100, 2)
+print(acc_rf_red)
+print('oob score:', round(rf_red.oob_score_, 4)*100)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# tune the hyper parameters:
+#-----------------------------------------------------------------------------------------------------------------------
+#from sklearn.model_selection import GridSearchCV, cross_val_score
+#param_grid = {"criterion":["gini", "entropy"],
+#              "min_samples_leaf":[1,5,10,25,50,70],
+#              "min_samples_split":[2,4,10,12,16,18,25,35],
+#              "n_estimators":[100, 400, 700, 1000, 1500]}
+
+#rf_clf = RandomForestClassifier(n_estimators=100, max_features='auto', oob_score=True,
+#                                random_state=1, n_jobs=-1)
+
+#clf = GridSearchCV(estimator=rf, param_grid=param_grid, n_jobs=-1)
+#clf.fit((X_train, Y_train))
+#print(clf.best_params_)
 #-----------------------------------------------------------------------------------------------------------------------
 # run model against test data and save output against passenger id
 #-----------------------------------------------------------------------------------------------------------------------
-test_X = processed[processed.set == "test"].drop(["survived", "set"], axis = 1)
-print(type(titanic))
+test_X = processed[processed.set == "test"].drop(["survived", "set", "Fare_high",
+                                                  "Fare_highest", "Ms", "deck_T",
+                                                  "deck_F", "deck_G"], axis = 1)
+#print(type(titanic))
 
 out = titanic[["PassengerId"]][titanic.set == "test"].copy()
-out.loc[:,"Survived"] = mod.predict(test_X)
+out.loc[:,"Survived"] = rf_red.predict(test_X)
 
 out.to_csv("titanic/titanic_submission.csv", index = False)
 
